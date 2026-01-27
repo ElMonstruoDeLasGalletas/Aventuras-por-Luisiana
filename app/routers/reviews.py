4b6ef5797343785a51209eb32ce70428de75b38e
dcarr <dcarr@example.com>
2026-01-27 16:54:54 +0100

    Cambiados Poi por Route para reviews de routers

--- app/routers/reviews.py
+++ app/routers/reviews.py
@@ -3,25 +3,25 @@ from sqlalchemy.orm import Session
 from typing import Optional, List
 
 from ..deps import get_db, get_current_user
-from ..models import Review, User, POI
+from ..models import Review, User, Route
 from ..schemas import ReviewCreate, ReviewOut
 
 router = APIRouter(prefix="/route/{route_id}/reviews", tags=["reviews"])
 
 @router.post("", response_model=ReviewOut)
 def create_review(
-    poi_id: int,
+    route_id: int,
     data: ReviewCreate,
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user),
 ):
-    poi = db.query(POI).filter(POI.id==poi_id, POI.is_deleted==False).first() #noqa
-    if not poi:
-        raise HTTPException(status_code=404, detail="POI not found")
+    route = db.query(Route).filter(Route.id==route_id, Route.is_deleted==False).first() #noqa
+    if not route:
+        raise HTTPException(status_code=404, detail="Route not found")
 
     review = Review(
         user_id=data.user_id,
-        poi_id=data.poi_id,
+        route_id=data.route_id,
         rating=data.rating,
         content=data.content,
         is_deleted=data.is_deleted
@@ -33,12 +33,12 @@ def create_review(
 
 @router.get("", response_model=List[ReviewOut])
 def list_reviews(
-    poi_id: int,
+    route_id: int,
     db: Session = Depends(get_db),
 ):
     return (
             db.query(Review)
-            .filter(Review.poi_id == poi_id, Review.is_deleted == False) #noqa
+            .filter(Review.route_id == route_id, Review.is_deleted == False) #noqa
             .order_by(Review.id.desc())
             .limit(200)
             .all()
